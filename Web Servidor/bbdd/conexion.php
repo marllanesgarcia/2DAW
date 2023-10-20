0532f0919023eb0bebd38f4060ee3d462cac7366
marllanesgarcia <sakiro1999@gmail.com>
2023-10-20 14:23:22 +0200

    Create conexion.php

---
+++ Web Servidor/bbdd/conexion.php
@@ -0,0 +1,17 @@
+<?
+
+// msqli = clase de conexion a una bbdd
+
+// $conexion = new mysqli("host","usuario","clave","bbdd")
+
+$conexion = new msqli("localhost","root","","cenec");
+
+if($conexion -> connect_errno){  // registra el codigo de error que hubo
+    echo "Error de conexion";
+}else{
+    echo "La conexion se ha realizado sin problemas";
+}
+
+$conexion -> close();
+
+?>
