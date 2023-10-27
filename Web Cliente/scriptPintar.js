732abb89c7ae012af1b5221b4c511595067a3745
marllanesgarcia <sakiro1999@gmail.com>
2023-10-27 11:00:58 +0200

    Update scriptPintar.js

--- Web Cliente/scriptPintar.js
+++ Web Cliente/scriptPintar.js
@@ -1,33 +1,46 @@
-"use script";
+"use strict";
 
-/* document.addEventListener("DOMContentLoaded", function () {
-    const cuadricula = document.getElementById("cuadricula");
-    const colorPicker = document.getElementById("color");
-  
-    // Crear la cuadrícula en la tabla
+
+var tabla = document.getElementById("cuadricula");
+var colorSelect = 'white'; //color inicial
+
+
+function pintar() {
     for (let i = 0; i < 20; i++) {
-      const row = document.createElement("tr");
-      for (let j = 0; j < 20; j++) {
-        const cell = document.createElement("td");
-        cell.classList.add("square");
-        cell.addEventListener("click", function () {
-          cell.style.backgroundColor = colorPicker.value;
-        });
-        row.appendChild(cell);
-      }
-      cuadricula.appendChild(row);
-    }
-  }); */
-
-  var tabla = document.getElementById("cuadricula");
-
-  function pintarCuadricula(){
-    for (let i=0;i<20;i++){}
-    let cuadricula=document.createElement("tr");
-    tabla.appendChild(cuadricula);
-        for(let j=0;j<2;j++){
-            let cuadricula2=document.createElement("th");
-            cuadricula2.style="width:100px;height:20px;border-style:solid;border-width:2px;border-color:black;";
+        let cuadricula = document.createElement("tr"); //crea la fila
+        tabla.appendChild(cuadricula);
+        for (let j = 0; j < 20; j++) {
+            let cuadricula2 = document.createElement("td");  //crea la celda
+            cuadricula.appendChild(cuadricula2);
+            cuadricula2.style = "width:100px;height:20px;border-style:solid;border-width:2px;border-color:black;";
+            cuadricula2.addEventListener("mouseover", function () {
+                cuadricula2.style.backgroundColor = colorSelect;
+            });
         }
-  }
-  pintarCuadricula();
+
+
+
+
+    }
+}
+pintar();
+
+
+function cambiarColor(color) {
+    colorSelect = color;
+}
+var botonera = document.getElementById("botonera");
+var colores = ['red', 'green', 'blue', 'pink', 'yellow', 'white'];
+
+
+colores.forEach(color => {
+    let boton = document.createElement("button");
+    boton.textContent = color;
+    boton.addEventListener('click', () => cambiarColor(color));
+    botonera.appendChild(boton);
+    boton.style = "width:100px;text-align:center";
+    boton.style.backgroundColor = color;
+
+
+})
+
