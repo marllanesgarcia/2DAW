8b777752d90c2e69547f0eea93c14d0be0181978
marllanesgarcia <sakiro1999@gmail.com>
2023-11-02 11:49:34 +0100

    js

--- Web Cliente/preguntasCuestionario.js
+++ Web Cliente/preguntasCuestionario.js
@@ -1,84 +1,110 @@
 "use strict";
-
+var boton = document.getElementById("siguiente");
 var p = document.getElementById("pregunta");
+var respuesta = document.getElementById("respuesta");
+var contador = 0;
+var preguntaActual = 0;
+var texto=document.getElementById("texto");
+var tiempoRestante = 0;
+var intervalID = null;
 
 var preguntas = [
     { //1
-        pregunta: "¿Cuál es el felino más grande del mundo?",
-        respuesta: "tigre"
+        p: "¿Cuál es el felino más grande del mundo?"
       },
       { // 2
-        Pregunta: "¿Cuál es la esperanza de vida promedio de un gato doméstico?",
-        Respuesta: "15 años"
+        p: "¿Cuál es la esperanza de vida promedio de un gato doméstico?"
       },
       { // 3
-        Pregunta: "¿Qué raza de gato tiene pelaje rizado?",
-        Respuesta: "gato rex"
+        p: "¿Qué raza de gato tiene pelaje rizado?"
       },
       { // 4
-        Pregunta: "¿Qué raza de gato tiene orejas grandes y puntiagudas?",
-        Respuesta: "gato siamés"  
+        p: "¿Qué raza de gato tiene orejas grandes y puntiagudas?"
       },
       { // 5
-        Pregunta: "¿Qué parte del cuerpo de un gato utiliza para equilibrarse?",
-        Respuesta: "cola"
+        p: "¿Qué parte del cuerpo de un gato utiliza para equilibrarse?",
       },
       { // 6
-        Pregunta: "¿Cómo se llama el proceso de limpieza que realizan los gatos con su lengua áspera?",
-        Respuesta: "grooming"
+        p: "¿Cómo se llama el proceso de limpieza que realizan los gatos con su lengua áspera?"
       },
       { // 7
-        Pregunta: "¿Cuál es el nombre científico del gato doméstico?",
-        Respuesta: "felis catus"
+        p: "¿Cuál es el nombre científico del gato doméstico?"
       },
       { // 8
-        Pregunta: "¿Cuál es el gato más grande registrado en la historia?",
-        Respuesta: "gato maine coon"
+        p: "¿Cuál es el gato más grande registrado en la historia?"
       },
       { // 9
-        Pregunta: "¿Qué raza de gato es conocida por tener un pelaje completamente negro?",
-        Respuesta: "gato bombay"
+        p: "¿Qué raza de gato es conocida por tener un pelaje completamente negro?"
       },
       { // 10
-        Pregunta: "¿Qué país se asocia comúnmente con el gato de la raza Manx?",
-        Respuesta: "isla de man"
+        p: "¿Qué país se asocia comúnmente con el gato de la raza Manx?"
       }];
 
-var intervalID = setInterval(cuestionario(),1000);
+var respuestas = [{respuesta: "tigre"},{respuesta:"15 años"},{respuesta: "gato rex"},
+    {respuesta: "gato rex"},{respuesta: "cola"},{respuesta: "grooming"},
+    {respuesta: "felis catus"},{respuesta: "gato maine coon"},{respuesta: "gato bombay"},{respuesta: "isla de man"}];
 
-function preguntasRandom(){
-
-for (var i=preguntas.length-1 ; i>0 ; i--){
-        var x = Math.floor(Math.random() * (i + 1));
-        [preguntas[x], preguntas[i]] = [preguntas[i], preguntas[x]];
-        console.log(preguntas);
-    }};
-
-function cuestionario() {
-    preguntasRandom();
-    var contador = 0;
-    var puntos = (contador / preguntas.length);
-
-for (var j = 0; j < preguntas.length; j++) {
-    var preguntaActual = preguntas[j].pregunta;
-    var respuestaCorrecta = preguntas[j].respuesta;
-
-    var respuestaU = document.getElementById("pregunta").textContent=preguntaActual;
-
-if (respuestaU && respuestaU.toLowerCase() === respuestaCorrecta.toLowerCase()) {
+    function preguntasRandom() {
+        for (var i = preguntas.length - 1; i > 0; i--) {
+            var x = Math.floor(Math.random() * (i + 1));
+            [preguntas[x], preguntas[i]] = [preguntas[i], preguntas[x]];
+            [respuestas[x], respuestas[i]] = [respuestas[i], respuestas[x]];
+        }
+    }
+    
+    function mostrarPregunta() {
+      if (preguntaActual < preguntas.length) {
+         //   iniciarContador(30);
+            p.textContent = preguntas[preguntaActual].p;
+            texto.value = "";
+        } else {
+            if (contador >= 5) {
+                alert( "¡GANASTE SUUUUUUUUUUUU!");
+            } else {
+                alert("Nah, perdiste");
+                location.reload();
+            }
+        }
+    }
+    
+function verificarRespuesta() {
+    var respuestaUsuario = respuesta.value;
+    if (respuestaUsuario.toLowerCase() === respuestas[preguntaActual].respuesta.toLowerCase()) {
         contador++;
-        document.getElementById("respuesta").textContent = "¡Has Acertado! ¡Bieeeen!";
-} else {
-    document.getElementById("respuesta").textContent = "Noup, te equivocaste, wey";
+        texto.textContent = "¡Has Acertado! ¡Bieeeen!";
+    } else {
+        texto.textContent = "Noup, te equivocaste, wey";
     }
-    }
-
-if (puntos >= 5) {
-      console.log("GANASTE SUUUUUUUUUUUU").alert("GANASTE OLEEEEEEEEEEE");
-} else {
-    //  console.log("Nah, perdiste").prompt("PERDISTE");
-      mostrarPreguntas(); 
+    preguntaActual++;
+    mostrarPregunta();
 }
+    
+    mostrarPregunta();
+    
+    boton.addEventListener("click", function () {
+        verificarRespuesta();
+        respuesta.value="";
+    });
+
+// esto pa los segundos
+
+/* function iniciarContador(segundos) {
+    tiempoRestante = segundos;
+    actualizarContador();
+    intervalID = setInterval(function() {
+        tiempoRestante--;
+        if (tiempoRestante <= 0) {
+            clearInterval(intervalID);
+            alert("Se acabó el tiempo. Continúa con la siguiente pregunta.");
+            preguntaActual++;
+            mostrarPregunta();
+        } else {
+            actualizarContador();
+        }
+    }, 1000)
 }
 
-mostrarPreguntas();
+function actualizarContador() {
+    var contadorElement = document.getElementById("contador");
+    contadorElement.textContent = "Tiempo que te queda: " + tiempoRestante + " segundos";
+} */
