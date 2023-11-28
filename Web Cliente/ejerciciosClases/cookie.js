31e9d497404800411d16e7cd5eafbd0c7bf29a40
marllanesgarcia <sakiro1999@gmail.com>
2023-11-28 12:03:56 +0100

    js

--- Web Cliente/ejerciciosClases/cookie.js
+++ Web Cliente/ejerciciosClases/cookie.js
@@ -20,11 +20,71 @@ for (var i=0; i<arrayCookies.length; i++){
 console.log(cookies);
  */
 
-var boton1 = document.getElementById("morado");
-var boton2 = document.getElementById("naranja");
-var boton3 = document.getElementById("amarillo");
 
- function cambiarColor(color) {
+document.addEventListener('DOMContentLoaded',function(){
+
+    var boton1 = document.getElementById("morado")
+  
+    var boton2 = document.getElementById("naranja");
+    var boton3 = document.getElementById("amarillo");
+
+    boton1.addEventListener('click', function () {
+        cambiarColor('rgb(175, 109, 236)');
+    });;
+
+    boton2.addEventListener('click', function () {
+        cambiarColor('rgb(241, 169, 105)');
+    });
+
+    boton3.addEventListener('click', function () {
+        cambiarColor('rgb(248, 248, 168)');
+    });
+
+    var cookies = document.cookie .split(";");
+    for (var i=0; i<cookies.length; i++){
+        //aqui
+            var prueba=cookies[i].split("=");
+            console.log("aqui entro yo");
+            console.log(prueba+"esta es la prueba");
+        if(prueba[0]=="color"){
+            console.log(prueba);
+            prueba[1]="purple";
+        }
+    }
+})
+
+function cambiarColor(color){
+    console.log("entra");
+    console.log(color);
+    document.body.style.backgroundColor=color;
+    var fecha = new Date ("2023-12-12").toGMTString();
+    document.cookie = "color="+color+";expires="+fecha;
+    var imprimir = document.cookie;
+
+}
+
+/* 
+    var boton1 = document.getElementById("morado")
+  
+    var boton2 = document.getElementById("naranja");
+    var boton3 = document.getElementById("amarillo");
+
+
+
+    boton1.addEventListener('click', function () {
+        cambiarColor('rgb(175, 109, 236)');
+    });;
+
+    boton2.addEventListener('click', function () {
+        cambiarColor('rgb(241, 169, 105)');
+    });
+
+    boton3.addEventListener('click', function () {
+        cambiarColor('rgb(248, 248, 168)');
+    });
+
+
+function cambiarColor(color) {
     document.body.style.backgroundColor = color;
     console.log("Color seleccionado:", color);
  
@@ -36,45 +96,6 @@ var boton3 = document.getElementById("amarillo");
     console.log("hola aqui estoy")
 }
 
-boton1.addEventListener('click', function () {
-    cambiarColor('rgb(175, 109, 236)');
-});
-
-boton2.addEventListener('click', function () {
-    cambiarColor('rgb(241, 169, 105)');
-});
-
-boton3.addEventListener('click', function () {
-    cambiarColor('rgb(248, 248, 168)');
-});
-
-/*function obtenerColorDeCookie() {
-    var cookies = document.cookie.split(';');
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i].trim();
-        if (cookie.startsWith("colorFondo=")) {
-            return cookie.substring("colorFondo=".length);
-        }
-    }
-    return null;
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    var colorGuardado = obtenerColorDeCookie();
-    document.cookie = "colorFondo=" +  document.body.style.backgroundColor;
-    if (colorGuardado) {
-        cambiarColor(colorGuardado);
-    }
-});
-
- var cookies = document.cookie;
-var arrayCookies = cookies.split(";");
-for (var i = 0; i < arrayCookies.length; i++) {
-    var co = arrayCookies[i].trim();
-    console.log(co + "-");
-}
-console.log(cookies); */
-
 var cookie = document.cookie;
 
 var fecha = new Date("2023-12-12").toGMTString(); 
@@ -84,4 +105,4 @@ for (var i = 0; i < cookiesArray.length; i++) {
     console.log("hola aqui estoy x 2")
     var co = cookiesArray[i].trim();
     console.log(co + "-");
-}
+} */
