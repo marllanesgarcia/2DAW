85a3900879b65ce19f66ebf36819d5504eb0bba4
marllanesgarcia <sakiro1999@gmail.com>
2023-11-29 13:19:51 +0100

    JS

---
+++ Web Servidor/ejercicioTiendaVirtual/comprar.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <title>Ama(tu)zon(a)</title>
+</head>
+<body>
+<?php
+    session_start();
+
+if (isset($_SESSION['nombre'])) {
+    $nombre_usuario = $_SESSION['nombre'];
+    echo "<h5 style='display:flex; justify-content:center;'>Bienvenido, $nombre_usuario!</h5>";
+    echo '<div style="display: flex; justify-content: center; background-color: #7d75bd; padding: 10px; justify-content: space-between;">
+             <a href="index.php">Inicio</a>
+             <a href="registrarse.php">Registrarte</a>
+             <a href="comprar.php">🛒</a>
+             <a href="codigoCerrarSesion.php">Cerrar sesión</a>
+          </div>';
+} else {
+    $nombre_usuario = "Invitado";
+    echo "<h5 style='display:flex; justify-content:center;'>Bienvenido, $nombre_usuario!</h5>";
+    echo '<div style="display: flex; justify-content: center; background-color: #7d75bd; padding: 10px; justify-content: space-between;">
+             <a href="index.php">Inicio</a>
+             <a href="registrarse.php">Registrarse</a>
+             <a href="comprar.php">🛒</a>
+             <a href="login.php">Iniciar sesión</a>
+             
+          </div>';
+}
+?> 
+
+            <h1 id="titulo">Ama(tu)zon(a) 🐦 </h1>
+
+
+
+
+            <footer> Informacion Legal</footer>
+</body>
+</html>
