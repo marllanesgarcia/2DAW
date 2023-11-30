6b305e60f9f3e93d68d231c0ae90e960e020943b
marllanesgarcia <sakiro1999@gmail.com>
2023-11-30 10:33:17 +0100

    js

---
+++ Web Cliente/ejerciciosClases/colores.js
@@ -0,0 +1,30 @@
+"use strict";
+
+function addItemToList(){
+    var elementos = document.getElementById("listItem")[0].value;
+    
+}
+
+function applyPreferences() {
+
+   /*  var colorElegido = document.getElementById("bgColor").value;
+    console.log(colorElegido);
+    sessionStorage.setItem("bgColor", colorElegido);
+
+    document.body.style.backgroundColor = sessionStorage.getItem("bgColor");
+
+    var tamano = document.getElementById("fontSize").value;
+    console.log(tamano);
+
+    document.body.style.fontSize = tamano+"px";  
+}
+if(sessionStorage.getItem("bgColor")!=null){
+    document.body.style.backgroundColor=sessionStorage.getItem("bgColor"); */
+
+    var estilo = document.getElementById("option").value;
+
+}
+
+
+
+
