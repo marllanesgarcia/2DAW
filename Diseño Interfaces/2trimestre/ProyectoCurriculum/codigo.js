6d5d8e9f89b83ebbfc90fb91252d0774cd1986d5
marllanesgarcia <sakiro1999@gmail.com>
2024-02-05 10:47:11 +0100

    js

--- Diseño Interfaces/2trimestre/ProyectoCurriculum/codigo.js
+++ Diseño Interfaces/2trimestre/ProyectoCurriculum/codigo.js
@@ -2,55 +2,59 @@
 
 var menu = document.getElementById("menu");
 var contenido = document.querySelectorAll("class")[0];
-
+var menu = document.getElementById("menu");
 
 
 document.addEventListener('DOMContentLoaded', function () {
     /* Esta funcion sirve para mover el muñeco por la pantalla*/
-    document.addEventListener('keydown', function(event) {
-    var element = document.getElementById('cuadrado');
-    var left = parseInt(element.style.left) || 0;
-    var top = parseInt(element.style.top) || 0;
-    var step = 10; 
-    // El muñequito se mueve dependiendo de la cantidad de pixeles
+    document.addEventListener('keydown', function (event) {
 
-    switch (event.key) {
-        case 'ArrowUp':
-            top -= step;
-        break;
-        case 'ArrowDown':
-            top += step;
-        break;
-        case 'ArrowLeft':
+        var element = document.getElementById('cuadrado');
+        var left = parseInt(element.style.left) || 0;
+        var top = parseInt(element.style.top) || 0;
+        var step = 10;
+
+        // El muñequito se mueve dependiendo de la cantidad de pixeles
+
+        switch (event.key) {
+            case 'ArrowUp':
+                top -= step;
+                break;
+            case 'ArrowDown':
+                top += step;
+                break;
+            case 'ArrowLeft':
                 left -= step
-        break;
-        case 'ArrowRight':
-            left += step;
-        break;
-    }
+                break;
+            case 'ArrowRight':
+                left += step;
+                break;
+        }
 
-    element.style.left = left + 'px';
-    element.style.top = top + 'px';
+        element.style.left = left + 'px';
+        element.style.top = top + 'px';
     })
 
-    boton.addEventListener("click", function (event) {
-        event.preventDefault();
-        
-            errorCita.textContent = "";
-            /* Aqui se muestra una alerta utilizando la biblioteca SweetAlert */
-            Swal.fire({
-                title: "La información es correcta.",
-                text: "Nombre de la reserva: " + nombre.value +
-                    "\nFecha de la reserva: " + fecha.value +
-                    "\nHora de la reserva: " + hora.value +
-                    "\nTipo de cita: " + selectTipoCita.value +
-                    "\n¡Continuar!",
-                imageUrl: "https://i.pinimg.com/originals/cc/4e/13/cc4e13c7aa8896b068576783e9c379dd.gif",
-                imageWidth: 400,
-                imageHeight: 200,
-                imageAlt: "imagen del gato"
-            });
+    menu.addEventListener("click", function () {
+        Swal.fire({
+            title: "Intrucciones de movimiento.",
+            html: `Usa las flechas para moverte: </br>` +
+                `\nFlecha derecha ⮕ : Moverte hacia la derecha.</br>` +
+                `\nFlecha izquierda ⬅ : Moverte hacia la izquierda. </br>` +
+                `\nFlecha arriba ⬆ : Moverte hacia arriba. </br>` +
+                `\nFlecha abajo ⬇ : Moverte hacia abajo.`,
+            imageUrl: "https://www.icegif.com/wp-content/uploads/2023/12/icegif-939.gif",
+            imageWidth: 110,
+            imageHeight: 110,
+            imageAlt: "imagen intrucciones"
+        });
     });
+
+    
+
+
+
+
 });
 
 
