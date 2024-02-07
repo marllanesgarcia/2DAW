e58a7eb931082a76d559592438a1300469bf07f3
marllanesgarcia <sakiro1999@gmail.com>
2024-02-07 14:18:42 +0100

    php

---
+++ Web Servidor/segundoTimestre/LARAVEL/mi_primera_app/app/Http/Controllers/TasksController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    public function home(){
+        return "Que bonita la frase que pongo";
+    }
+}
---
+++ Web Servidor/segundoTimestre/LARAVEL/mi_primera_app/resources/views/ejemplo.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Ejemplo</title>
+ <style>
+    h1 {
+    color: green;
+    }
+ </style>
+</head>
+<body>
+ <h1>Ejemplo de prueba de controladores</h1>
+
+ <h3>¡Esta funcionando!</h3>
+</body> 
