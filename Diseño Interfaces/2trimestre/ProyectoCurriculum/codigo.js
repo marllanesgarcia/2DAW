8b8ba148caf59ca96592d612b6e5b9058d4605c3
marllanesgarcia <sakiro1999@gmail.com>
2024-02-22 09:40:11 +0100

    js

--- Diseño Interfaces/2trimestre/ProyectoCurriculum/codigo.js
+++ Diseño Interfaces/2trimestre/ProyectoCurriculum/codigo.js
@@ -45,15 +45,14 @@ document.addEventListener('DOMContentLoaded', function () {
                     html: 
                     `<h1>Informacion General</h1>
                     <h2> Mar Llanes García.</h2>
-                    <ul style='list-style-type: none;'> 
-                        <li style='text-align: left;'>Fecha de nacimiento: 26/01/1999</li>		
-                        <li style='text-align: left;>Nacionalidad: Española</li>	
-                        <li style='text-align: left;>Número de teléfono: (+34) 682539169 (Móvil)</li>	
-                        <li style='text-align: left;>Dirección de correo electrónico: sakiro1999@gmail.com</li>
-                        <li style='text-align: left;>Dirección: Calle Magnolia 4, Casa 2, 29639, España (Domicilio)</li>
-                        <li style='text-align: left;>Idiomas: Español, Inglés, Coreano</li>
+                    <ul style='list-style-type: none'> 
+                        <li style='text-align: left'>Fecha de nacimiento: 26/01/1999</li>		
+                        <li style='text-align: left'>Nacionalidad: Española</li>	
+                        <li style='text-align: left'>Número de teléfono: (+34) 682539169 (Móvil)</li>	
+                        <li style='text-align: left'>Dirección de correo electrónico: sakiro1999@gmail.com</li>
+                        <li style='text-align: left'>Dirección: Calle Magnolia 4, Casa 2, 29639, España (Domicilio)</li>
+                        <li style='text-align: left'>Idiomas: Español, Inglés, Coreano</li>
                     </ul>
-                    </br>
                     <h3> Aspectos a Destacar </h3>
                     <p> Trabajo en equipo | Responsabilidad en el trabajo | 
                     Capacidad de aprendizaje rápido | Resolución de problemas organización y puntualidad
@@ -62,7 +61,9 @@ document.addEventListener('DOMContentLoaded', function () {
                     `,
                     confirmButtonText: '¡Seguimos!',
                     confirmButtonColor: '#8E44AD',
-                    background: '#F4ECF7'
+                    background: '#F4ECF7',
+                    grow: 'row'
+
                   });
             }
         });
@@ -74,25 +75,26 @@ document.addEventListener('DOMContentLoaded', function () {
                 `
                 <h1>Experiencia Laboral</h1>
                 </br>
-                <ul style='list-style-type: none;'> 
-                    <li style='text-align: left;'>Ayudante de camarera en barra y coctelera “VULCANO GRILL” (01/07/2021 - 17/09/2023)</li>
+                <ul style='list-style-type: none'> 
+                    <li style='text-align: left'>Ayudante de camarera en barra y coctelera “VULCANO GRILL” (01/07/2021 - 17/09/2023)</li>
                     </br>		
-                    <li style='text-align: left;>Prácticas en escuela de idiomas “CULTURA ASIÁTICA” (1/04/2022 - 30/05/20232)</li>	
+                    <li style='text-align: left'>Prácticas en escuela de idiomas “CULTURA ASIÁTICA” (1/04/2022 - 30/05/20232)</li>	
                     </br>
-                    <li style='text-align: left;>Ayudante de camarera en barra “LA FÁBRICA” (12/2018)</li>	
+                    <li style='text-align: left'>Ayudante de camarera en barra “LA FÁBRICA” (12/2018)</li>	
                     </br>
-                    <li style='text-align: left;>Ayudante de camarera en barra y Camarera en Coctelería “EL TALLER”  (01/06/2021 - 30/09/2021)</li>
+                    <li style='text-align: left'>Ayudante de camarera en barra y Camarera en Coctelería “EL TALLER”  (01/06/2021 - 30/09/2021)</li>
                     </br>
-                    <li style='text-align: left;>Puesto de venta asiático SALONES MANGA EN CONGRESOS (01/08/2017 - 30/09/2018)</li>
+                    <li style='text-align: left'>Puesto de venta asiático SALONES MANGA EN CONGRESOS (01/08/2017 - 30/09/2018)</li>
                     </br>
-                    <li style='text-align: left;>Camarera sala y terraza “TUKTUK NOODLES” (01/07/2019 - 30/08/2019)</li>
+                    <li style='text-align: left'>Camarera sala y terraza “TUKTUK NOODLES” (01/07/2019 - 30/08/2019)</li>
                     </br>
-                    <li style='text-align: left;'>Diseño de camisetas hechas a mano TIENDA ONLINE PROPIA (1/06/2017 - 30/06/2022)</li>
+                    <li style='text-align: left'>Diseño de camisetas hechas a mano TIENDA ONLINE PROPIA (1/06/2017 - 30/06/2022)</li>
                 </ul>
                 `,
                 confirmButtonText: '¡Seguimos!',
                 confirmButtonColor: '#935116',
-                background: '#FAE5D3'
+                background: '#FAE5D3',
+                grow: 'row'
               });
            
         }
@@ -105,19 +107,20 @@ document.addEventListener('DOMContentLoaded', function () {
                 `
                 <h1>Formacion Academica</h1>
                 </br>
-                <ul style='list-style-type: none;'> 
-                    <li style='text-align: left;'>FPIGS DESARROLLO DE APLICACIONES WEB C.D.P. CENEC-MALAGA (01/10/22 - 21/06/2024)</li>		
+                <ul style='list-style-type: none'> 
+                    <li style='text-align: left'>FPIGS DESARROLLO DE APLICACIONES WEB C.D.P. CENEC-MALAGA (01/10/22 - 21/06/2024)</li>		
                     </br>
-                    <li style='text-align: left;>GRADO EN ESTUDIOS DE ASIA ORIENTAL MENCION COREA UMA Málaga, España (15/09/2018 - 20/06/2022)</li>	
+                    <li style='text-align: left'>GRADO EN ESTUDIOS DE ASIA ORIENTAL MENCION COREA UMA Málaga, España (15/09/2018 - 20/06/2022)</li>	
                     </br>
-                    <li style='text-align: left;>BACHILLERATO DE ARTES PLÁSTICAS IES Cerro del Viento (15/09/2015 - 20/06/2017)</li>	
+                    <li style='text-align: left'>BACHILLERATO DE ARTES PLÁSTICAS IES Cerro del Viento (15/09/2015 - 20/06/2017)</li>	
                     </br>
-                    <li style='text-align: left;>CPM COSTA DEL SOL Conservatorio profesional de música (15/09/2009 - 20/06/2015)</li>
+                    <li style='text-align: left'>CPM COSTA DEL SOL Conservatorio profesional de música (15/09/2009 - 20/06/2015)</li>
                     </ul>
                 `,
                 confirmButtonText: '¡Seguimos!',
                 confirmButtonColor: '#0E6655',
-                background: '#D0ECE7'
+                background: '#D0ECE7',
+                grow: 'row'
               });
         }
     });
@@ -129,14 +132,14 @@ document.addEventListener('DOMContentLoaded', function () {
                 `
                 <h1>Proyecto y Repositorios</h1>
                 </br>
-                <ul style='list-style-type: none;'> 
-                    <li style='text-align: left;'>A continuación se muestra el link de GitHub con todos los proyectos realizados y algunos a destacar.</li>		
+                <ul style='list-style-type: none'> 
+                    <li style='text-align: left'>A continuación se muestra el link de GitHub con todos los proyectos realizados y algunos a destacar.</li>		
                     </br>
-                    <li style='text-align: left;> Usuario de GitHub: marllanesgarcia ( <a>https://github.com/marllanesgarcia</a> ) </li>
+                    <li style='text-align: left'> Usuario de GitHub: marllanesgarcia ( <a href='https://github.com/marllanesgarcia'>https://github.com/marllanesgarcia</a> ) </li>
                     </br>
-                    <li style='text-align: left;> Proyecto Final Primer año de DAW : <a>https://marllanesgarcia.github.io/ProyectoGatuno/index.html</a></li>		
+                    <li style='text-align: left'> Proyecto Final Primer año de DAW : <a href= 'https://marllanesgarcia.github.io/ProyectoGatuno/index.html' target='_blank' >https://marllanesgarcia.github.io/ProyectoGatuno/index.html</a></li>		
                     </br>
-                    <li style='text-align: left;> Página sobre League of Legends: <a>https://marllanesgarcia.github.io/ejercicios/index.html</a> </li>	
+                    <li style='text-align: left'> Página sobre League of Legends: <a href='https://marllanesgarcia.github.io/ejercicios/index.html' >https://marllanesgarcia.github.io/ejercicios/index.html</a> </li>	
                     </br>
                     
                     </ul>
@@ -144,7 +147,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 `,
                 confirmButtonText: '¡Seguimos!',
                 confirmButtonColor: '#935116',
-                background: '#FAE5D3'
+                background: '#FAE5D3',
+                grow: 'row'
               });
         }
     });
